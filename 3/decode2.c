f0381b37069d131dc20bdaeeb6b59cc9aa977ad3
jeyan <justin_yan_2@sfu.ca>
2021-02-09 21:49:22 -0800

    Add 3

---
+++ 3/decode2.c
@@ -0,0 +1,12 @@
+/decode2.c
+
+//x = rdi, y = rsi, z = rdx, return = rax
+long decode2(long x, long y, long z){
+	y -= z;
+	x *= y;
+	long temp = x;
+	temp = temp << 63;
+	temp = temp >> 63;
+	temp = temp ^ x;
+	return temp;
+}
